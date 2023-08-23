f776192432fa9192acec649c0e7ce8a41006374e
Camara Vamoro <cvamorocamson@gmail.com>
2023-08-23 22:03:25 +0100

    endOfProject

--- index.js
+++ index.js
@@ -4,12 +4,78 @@ const  grabBox = document.querySelector('.grabeBox')
 const cardNumber = document.getElementById('cardNumber')
 const validDate = document.getElementById('validDate')
 const names = document.getElementById('name')
+const ribPlace = document.querySelector('.ribPlace')
 
 //box to add Event 
 const CardGrab = document.getElementById('CardGrab')
 const cardDate = document.getElementById('cardDate')
 const ownerName = document.getElementById('ownerName')
+
+
+
+
+//flip effect 
+
+const forVerso = document.querySelector('.forVerso')
+const title = document.querySelector('.card .title')
+const imgPuce = document.querySelector('.card .imgRecto')
+const date = document.querySelector('.card .date')
 const ribBox = document.getElementById('ribBox')
+const card = document.querySelector('.card')
+
+
+CardGrab.addEventListener('focus', ()=>{
+    title.classList.remove('active')
+    imgPuce.classList.remove('active')
+    date.classList.remove('active')
+    forVerso.classList.remove('active')
+    card.classList.remove('active')
+    names.classList.remove('active')
+    validDate.classList.remove('active')
+    cardNumber.classList.remove('active')
+
+})
+
+ownerName.addEventListener('focus', ()=>{
+    title.classList.remove('active')
+    imgPuce.classList.remove('active')
+    date.classList.remove('active')
+    forVerso.classList.remove('active')
+    card.classList.remove('active')
+    names.classList.remove('active')
+    validDate.classList.remove('active')
+    cardNumber.classList.remove('active')
+
+})
+
+cardDate.addEventListener('focus', ()=>{
+    title.classList.remove('active')
+    imgPuce.classList.remove('active')
+    date.classList.remove('active')
+    forVerso.classList.remove('active')
+    card.classList.remove('active')
+    names.classList.remove('active')
+    validDate.classList.remove('active')
+    cardNumber.classList.remove('active')
+
+})
+
+
+ribBox.addEventListener('focus', ()=>{
+    title.classList.add('active')
+    imgPuce.classList.add('active')
+    date.classList.add('active')
+    forVerso.classList.add('active')
+    card.classList.add('active')
+    names.classList.add('active')
+    validDate.classList.add('active')
+    cardNumber.classList.add('active')
+
+})
+
+ribBox.addEventListener('input', ()=>{
+    ribPlace.innerText = ribBox.value
+})
 
 
 
@@ -20,12 +86,17 @@ newCardBtn.addEventListener('click', ()=>{
 
 CardGrab.addEventListener('input', ()=>{
     cardNumber.innerText = CardGrab.value
+
 })
 
 cardDate.addEventListener('input', ()=>{
-    const dateJ = cardDate.getDay
-    const dateM = cardDate.getMonth
-    validDate.innerText = `${dateJ}/${dateM}`
+    const dateInput = cardDate.value;
+    const selectedDate = new Date(dateInput);
+    const day = selectedDate.getDate();
+    const month = selectedDate.getMonth() + 1;
+
+    validDate.innerText = `${day}/${month}`;
+
 })
 
 ownerName.addEventListener('input', ()=>{
